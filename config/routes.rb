2a3755f8c8ebcbcbd35f6a7d85d831917c5fa2ba
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-27 15:38:15 -0400

    Setup Active admin (#7)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admin_users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   scope :api, defaults: { format: :json } do
     devise_for :users,
