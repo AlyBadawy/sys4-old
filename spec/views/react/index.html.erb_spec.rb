a205c4a8b5ca5c344781c927e928b26adbe2a0b7
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-27 12:50:17 -0400

    Setup React app

---
+++ spec/views/react/index.html.erb_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "react/index.html.erb" do
+  it "renders the page" do
+    render
+    expect(rendered).to match('<div id="react-root" />')
+  end
+end
