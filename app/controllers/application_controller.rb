802666519c75dde5f1b24cdc626e571028a3df63
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-27 13:57:42 -0400

    Setup Whodunnit
    Signed-off-by: Aly Badawy <AlyBadawy@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_paper_trail_whodunnit
+
+  def user_for_paper_trail
+    return current_admin_user.id if try(:current_admin_user)
+    return current_user.id if try(:current_user)
+
+    "Unknown user"
+  end
 end
