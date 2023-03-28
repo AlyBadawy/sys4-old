4b23b64ae3365d3a256118c66215e86be5ac000d
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-28 13:18:44 -0400

    jest failstep of CI/CD

--- app/javascript/jest/pass.test.tsx
+++ app/javascript/jest/pass.test.tsx
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom/extend-expect';
 describe('Test', () => {
   it('passes!', () => {
     render(<div>This should pass</div>);
-    const paragraph = screen.getByText('This should pass');
+    const paragraph = screen.getByText('This should fail');
     expect(paragraph).toBeInTheDocument();
   });
 });
