11e8ad832e3d79bc23e60bae57c8e9ab018812a3
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-03-28 13:25:53 -0400

    Finished the verify pipeline

--- app/javascript/jest/pass.test.tsx
+++ app/javascript/jest/pass.test.tsx
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom/extend-expect';
 describe('Test', () => {
   it('passes!', () => {
     render(<div>This should pass</div>);
-    const paragraph = screen.getByText('This should fail');
+    const paragraph = screen.getByText('This should pass');
     expect(paragraph).toBeInTheDocument();
   });
 });
