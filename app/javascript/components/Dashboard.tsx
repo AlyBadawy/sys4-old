de16f0c1dd9e54f42121293805c51b06021b76d3
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-04-03 08:28:03 -0400

    Added TailwindCsss

--- app/javascript/components/Dashboard.tsx
+++ app/javascript/components/Dashboard.tsx
@@ -5,5 +5,11 @@ export const Dashboard = () => {
   const { data, error, isLoading } = useGetStatusQuery();
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error!</div>;
-  return <div>{data && <>Status is: {JSON.stringify(data)}!</>}</div>;
+  return (
+    <>
+      <main className='container mx-auto mt-28 px-5 flex'>
+        {data && <>Status is: {JSON.stringify(data)}!</>}
+      </main>
+    </>
+  );
 };
