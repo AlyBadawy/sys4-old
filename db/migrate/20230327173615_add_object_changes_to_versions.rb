e6f76918b518d969df2a0015ccde7baf130ca26f
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-04-02 22:25:30 -0400

    Add Versions to Accounts in Active Admin (#28)

--- db/migrate/20230327173615_add_object_changes_to_versions.rb
+++ db/migrate/20230327173615_add_object_changes_to_versions.rb
@@ -4,11 +4,7 @@
 # will store the `changes` diff for each update event. See the readme for
 # details.
 class AddObjectChangesToVersions < ActiveRecord::Migration[7.0]
-  # The largest text column available in all supported RDBMS.
-  # See `create_versions.rb` for details.
-  TEXT_BYTES = 1_073_741_823
-
   def change
-    add_column :versions, :object_changes, :text, limit: TEXT_BYTES
+    add_column :versions, :object_changes, :jsonb
   end
 end
