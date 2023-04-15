9004a1f713a70be46a7a3922d062705b690218b4
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-04-14 20:18:39 -0400

    Fixed some dependencies

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,12 +72,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mailgun.org",
-    port: 587,
-    user_name: "postmaster@mg.sys4.dev",
-    password: Rails.application.credentials.mailgun_password!,
-  }
+  config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 
   # config/enviroments/development.rb
   config.action_mailer.preview_path = "#{Rails.root}/app/mailer_previews"
