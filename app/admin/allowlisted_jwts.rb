d11310bdfa041192a8131213589d7b4d3515b93c
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-04-15 23:01:16 -0400

    Add User agent, ip, and location to JWT

--- app/admin/allowlisted_jwts.rb
+++ app/admin/allowlisted_jwts.rb
@@ -6,4 +6,23 @@ ActiveAdmin.register AllowlistedJwt do
   actions :index, :show, :destroy
 
   config.filters = false
+
+  index do
+    selectable_column
+    id_column
+    column :agent
+    column :location
+    column :ip do |resource|
+      if resource.ip
+        link_to resource.ip, "http://who.is/whois-ip/ip-address/#{resource.ip}", target: "_blank", rel: "noopener"
+      else
+        status_tag "no", label: "No IP address"
+      end
+    end
+    column :created_at
+    column "Valid" do |resource|
+      resource.exp < Time.zone.now ? (status_tag "no", labebl: "Expired") : (status_tag "yes", labebl: "Valid")
+    end
+    actions
+  end
 end
