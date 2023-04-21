4db904b8395241861ce620939521a08339d8209f
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-04-21 13:08:31 -0400

    Fix not found page

--- spec/views/react/index.html.erb_spec.rb
+++ spec/views/react/index.html.erb_spec.rb
@@ -5,6 +5,6 @@ require "rails_helper"
 RSpec.describe "react/index.html.erb" do
   it "renders the page" do
     render
-    expect(rendered).to match('<div id="react-root" />')
+    expect(rendered).to match('<div id="root" />')
   end
 end
