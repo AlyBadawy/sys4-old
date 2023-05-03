7251d850036239522f812e4d81128b665639bfba
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-05-03 12:51:15 -0400

    completed

--- app/javascript/auth/LogOutButton.tsx
+++ app/javascript/auth/LogOutButton.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useAppDispatch } from '../store/store';
 import { logOut } from '../store/slices/UserSlice';
 import { useLogoutMutation } from '../store/api/UserApi';
-import { toast } from 'react-toastify';
 import { resetLayout } from '../store/slices/LayoutSlice';
 
 export const LogOutButton = () => {
@@ -11,11 +10,8 @@ export const LogOutButton = () => {
 
   const handleLogout = () => {
     void logout().unwrap;
-    dispatch(logOut());
     dispatch(resetLayout());
-    toast.info('You are now signed out', {
-      toastId: 'logout',
-    });
+    dispatch(logOut());
   };
 
   return (
